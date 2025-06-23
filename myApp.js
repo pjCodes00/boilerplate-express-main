6927a744b0a225dfebe423b1270937f5cdf2e54c
pooja <poojakaruvan@gmail.com>
2025-06-23 18:29:28 +0300

    send file

--- myApp.js
+++ myApp.js
@@ -3,8 +3,14 @@ let app = express();
 
 console.log('Hello World')
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
   res.send('Hello Express')
+})*/
+
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
 })
 
 
