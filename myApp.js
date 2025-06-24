e852592d6ed17dbf65ff582e34d6242b609ee027
pooja <poojakaruvan@gmail.com>
2025-06-24 10:03:27 +0300

    add middleware

--- myApp.js
+++ myApp.js
@@ -10,6 +10,10 @@ console.log('Hello World')
 
 app.use('/public', express.static(__dirname + '/public'))
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
+  next()
+})
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 })
